9acc508221c0ccd0e65b33c80eef9df529881fe4
chrisFFRR <chris@shotatdawn.net>
2019-10-12 16:42:32 +0200

    started top50Album layout

---
+++ Opus/ViewControllers/Top50AlbumCell.swift
@@ -0,0 +1,17 @@
+//
+//  Top50AlbumCell.swift
+//  Opus
+//
+//  Created by Christopher Marchand on 12/10/2019.
+//  Copyright © 2019 Christopher Marchand. All rights reserved.
+//
+
+import UIKit
+
+class Top50AlbumCell: UICollectionViewCell {
+   
+    
+    @IBOutlet var albumImage: UIImageView!
+    @IBOutlet var albumTitle: UILabel!
+    @IBOutlet var albumArtist: UILabel!
+}
